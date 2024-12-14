8f08b582b6dec0aa4ac39d6d64b2cd78ce9076fd
arshia <arshiaharati2@gmail.com>
2024-12-14 15:00:39 +0330

    new update

---
+++ models/user.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose")
+const {Shema} = mongoose;
+
+const userShema = mongoose.Shema({
+    name :{
+        type : String,
+        minlength : 3,
+        maxLength : 100,
+        required : true
+    },
+    userName :{
+        type : String,
+        minlength : 3,
+        maxLength : 200,
+        required : true
+    },
+    email : {
+        type : String,
+        minlength : 3,
+        maxlength : 320,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    phone : {
+        type : String,
+        minlength : 11,
+        maxlength : 11,
+        required : true,
+        match : [/09(1[0-9]|3[1-9]|2[1-9])-?[0-9]{3}-?[0-9]{4}/]
+    },
+    address : {
+        type :  String,
+        minlength : 3,
+        maxLength : 400,
+
+    }
+
+
+})
